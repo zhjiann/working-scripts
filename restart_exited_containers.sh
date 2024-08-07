127ed8a15846e16ce5c5ec89869d2904162acc93
zhjiann <zhjiann0106@gmail.com>
2024-08-07 15:33:33 +0800

    Update restart_exited_containers.sh

--- restart_exited_containers.sh
+++ restart_exited_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exit_count=$(docker ps -a | grep -c Exit)
+exit_count=$(docker ps -aq -f status=exited | wc -l)
 
 if [ "${exit_count}" -ne 0 ]; then
-    docker ps -a | grep Exited | awk '{print $NF}'  | xargs -n 1 -I {} bash -c 'docker start {} && echo "docker start {} > /data/br/logs/start_containers.log"'
+    docker ps -a -q -f status=exited | xargs -I {} bash -c 'docker start {} && echo "docker start {}" >> /data/br/logs/start_containers.log'
 fi
